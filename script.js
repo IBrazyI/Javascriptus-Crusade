e1cb63518e7bc7c1cd4fd82dd6cce51d6926c754
IBrazyI <73790113+IBrazyI@users.noreply.github.com>
2021-03-14 19:34:28 +0000

    added hover effect and descriptions to all factions, text is easy to read and effect fits well with background images

--- script.js
+++ script.js
@@ -60,10 +60,8 @@ function factionSelection() {
             document.getElementById("spacemarine-info").classList.remove('remove');
             document.getElementById("spacemarine-info").innerHTML = spacemarineDescription;
             document.getElementById("spacemarine-info").style.backgroundColor = "#b6b5b591";
-           
-
-
         };
+
         spaceMarine.onmouseleave = function(){
             document.getElementById("spacemarine-title").classList.remove('remove');
             document.getElementById("spacemarine-info").classList.add('remove');
@@ -106,6 +104,40 @@ function enemySelection() {
     let chaos = document.getElementById("chaos-image");
     let tyranids = document.getElementById("tyranids-image");
 
+    orks.onmouseenter = function(){
+        document.getElementById("orks-title").classList.add('remove');
+        document.getElementById("orks-info").classList.remove('remove');
+        document.getElementById("orks-info").innerHTML = orksDescription;
+        document.getElementById("orks-info").style.backgroundColor = "#b6b5b591";
+    };
+    
+    orks.onmouseleave = function(){
+        document.getElementById("orks-title").classList.remove('remove');
+        document.getElementById("orks-info").classList.add('remove');
+    };
+
+    chaos.onmouseenter = function(){
+        document.getElementById("chaos-title").classList.add('remove');
+        document.getElementById("chaos-info").classList.remove('remove');
+        document.getElementById("chaos-info").innerHTML = chaosDescription;
+        document.getElementById("chaos-info").style.backgroundColor = "#b6b5b591";
+    };
+    chaos.onmouseleave = function(){
+        document.getElementById("chaos-title").classList.remove('remove');
+        document.getElementById("chaos-info").classList.add('remove');
+    };
+
+    tyranids.onmouseenter = function(){
+        document.getElementById("tyranids-title").classList.add('remove');
+        document.getElementById("tyranids-info").classList.remove('remove');
+        document.getElementById("tyranids-info").innerHTML = tyranidsDescription;
+        document.getElementById("tyranids-info").style.backgroundColor = "#b6b5b591";
+    };
+    tyranids.onmouseleave = function(){
+        document.getElementById("tyranids-title").classList.remove('remove');
+        document.getElementById("tyranids-info").classList.add('remove');
+    };
+
     orks.addEventListener("click", enemySelected);
     chaos.addEventListener("click", enemySelected);
     tyranids.addEventListener("click", enemySelected);
@@ -140,5 +172,11 @@ let spacemarineDescription =
  "Spacemarines are the genetically enhanced super soldiers of the Imperium of man. Feared through the galaxy and one of humanity's greatest defenders, they know no fear wear thick ceramite armour that makes them almost invunrable to small arms fire."
 let admechDescription =
  "The Adeptus Mechanicus is one of the most powerful organisations in the Imperium. More machine than human, they field large number machine enhanced troops with technologically advanced weapons while thier gigantic warmachines stalk the battlefield."
- let guardDescription =
+let guardDescription =
   "The Guard forms the very backbone of the Imperium; without it, Mankind would surely perish. Fighting neither with the advantages of genetic enhancement or the most powerful personal weaponry, the Guard possesses the courage and the manpower to face and annihilate the enemies of the Emperor across the galaxy."
+let orksDescription =
+ "Orks are a warlike, crude, and highly aggressive green-skinned Xenos race. Although their society is entirely primitive and brutal, the Ork race is also the most successful species in the whole Galaxy, outnumbering possibly every other race. However, due to their aggressive and warlike nature, warring as much between themselves as against other races."
+let chaosDescription =
+ "The Chaos Space Marines were once loyal, superhuman warriors of the Emperor, but turned their backs on the Master of Mankind when his foremost son and Primarch, the Warmaster Horus, was corrupted by the Chaos Gods. Now, as champions of the Dark Gods infused with the infernal power of the warp, they seek only to destroy the very empire they once fought to build more than 10 thousand years ago."
+let tyranidsDescription =
+ "The Tyranids are an extragalactic alien race, whose sole purpose is the consumption of all forms of genetic and biological material in order to evolve and reproduce. Every function is carried out by living, engineered creatures, each of which collectively forms the Hive Fleet, directed by a single Hive Mind.The Tyranids are seen as one of the gravest threats to the entire Galaxy."
