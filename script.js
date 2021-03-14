0c4a438cba28cc94240c1b3cec168671061f8c07
IBrazyI <73790113+IBrazyI@users.noreply.github.com>
2021-03-14 14:03:47 +0000

    complied into page and title page functions into on function that plays the entire intro sequence and ends with the faction selection page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Intro Segemnt */
 
-function introPage(){
+function introSequence(){
 
     let text = document.querySelector(".intro-text");
     let strText = text.textContent;
@@ -30,11 +30,6 @@ function introPage(){
         let introBtn = document.getElementById("intro-btn");
         introBtn.classList.remove("hide");
     }
-}
-
-/* Title Page */
-
-function titlePage(){
 
     document.getElementById("intro-btn").addEventListener("click", revealTitle);
 
@@ -42,10 +37,20 @@ function titlePage(){
         document.getElementById("intro-page").classList.add('remove');
         document.getElementById("title-page").classList.remove('remove');
     };
+
+    document.getElementById("title-btn").addEventListener("click", revealFaction);
+
+    function revealFaction() {
+        document.getElementById("title-page").classList.add('remove');
+        document.getElementById("faction-selection").classList.remove('remove');
+        document.getElementById("header").classList.remove('remove');
+    };
 }
 
-/* titlePage()
-
+function factionSelection() {
+    
+}
+/*
 talkingHead()
 
 navBarMouseOver()
@@ -60,5 +65,4 @@ endScreen()
 
 emailSubmit() */
 
-introPage();
-titlePage();
+introSequence();
