06a80a3ed60475a70c63411a5b9cb1f9f49cc411
IBrazyI <73790113+IBrazyI@users.noreply.github.com>
2021-04-11 10:05:01 +0100

    appeneded each div into the forces container, then gave each container is own class for future styling

--- forces.js
+++ forces.js
@@ -13,3 +13,16 @@ const forcesContainer = document.createElement('div');
 const factionsContainer = document.createElement('div');
 const enemiesContainer = document.createElement('div');
 const informationContainer = document.createElement('div');
+
+/*Container Appends*/
+forcesContainer.appendChild(factionsContainer);
+forcesContainer.appendChild(enemiesContainer);
+forcesContainer.appendChild(informationContainer);
+
+/*Container Classes*/
+forcesContainer.classList.add('forces-container');
+factionsContainer.classList.add('factions-container');
+factionsContainer.classList.add('forces-selection');
+enemiesContainer.classList.add('eneimes-container');
+enemiesContainer.classList.add('focres-selection');
+informationContainer.classList.add('forces-info-container');
