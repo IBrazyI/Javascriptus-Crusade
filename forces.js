fd8d1d06bc56924028f520c73b35e2ec3de471ea
IBrazyI <73790113+IBrazyI@users.noreply.github.com>
2021-04-13 10:10:01 +0100

    changed the created of 'h2; elements for the forces selection page to 'p' also finnished styling for small screen sizes with correct positioning

--- forces.js
+++ forces.js
@@ -14,7 +14,13 @@ const factionsContainer = document.createElement('div');
 const enemiesContainer = document.createElement('div');
 const informationContainer = document.createElement('div');
 
+/*Forces Container Elements*/
+let forcesTitle = document.createElement('h1');
+forcesTitle.id = "forces-title";
+
 /*Container Appends*/
+document.body.appendChild(forcesContainer);
+forcesContainer.appendChild(forcesTitle);
 forcesContainer.appendChild(factionsContainer);
 forcesContainer.appendChild(enemiesContainer);
 forcesContainer.appendChild(informationContainer);
@@ -27,52 +33,66 @@ enemiesContainer.classList.add('eneimes-container');
 enemiesContainer.classList.add('focres-selection');
 informationContainer.classList.add('forces-info-container');
 
-/*Forces Container Elements*/
-let forcesTitle = document.createElement('h1');
+
+
 
 /*Faction Container Elements*/
 let factionSpaceMarines = document.createElement('div');
-factionSpaceMarines.classList.add("forces-Select-Image");
+factionSpaceMarines.classList.add("forces-select-image");
 factionSpaceMarines.id = "faction-space-marines";
-    let spaceMarinesLabel = document.createElement('h2');
-    spaceMarinesLabel.id = "space-marines-label";
+    let spaceMarinesLabel = document.createElement('p');
+    spaceMarinesLabel.classList.add('forces-label');
 let factionAdMech = document.createElement('div');
-factionAdMech.classList.add("forces-Select-Image");
+factionAdMech.classList.add("forces-select-image");
 factionAdMech.id = "faction-ad-mech";
-    let adMechLabel = document.createElement('h2');
-    adMechLabel.id = "ad-mech-label";
+    let adMechLabel = document.createElement('p');
+    adMechLabel.classList.add('forces-label');
 let factionGuard = document.createElement('div');
-factionGuard.classList.add("forces-Select-Image");
+factionGuard.classList.add("forces-select-image");
 factionGuard.id = "faction-guard";
-    let guardLabel = document.createElement('h2');
-    guardLabel.id = "guard-label";
+    let guardLabel = document.createElement('p');
+    guardLabel.classList.add('forces-label');
 
 /*Appends*/
+factionsContainer.appendChild(factionSpaceMarines);
+factionsContainer.appendChild(factionAdMech);
+factionsContainer.appendChild(factionGuard);
 factionSpaceMarines.appendChild(spaceMarinesLabel);
 factionAdMech.appendChild(adMechLabel);
 factionGuard.appendChild(guardLabel);
 
 /*Enemies Container Elements*/
 let factionOrks = document.createElement('div');
-factionOrks.classList.add("forces-Select-Image");
+factionOrks.classList.add("forces-select-image");
 factionOrks.id = "faction-orks";
-    let orksLabel = document.createElement('h2');
-    orksLabel.id = "orks-label";
-let factionChaos = documennt.createElement('div');
-factionChaos.classList.add("forces-Select-Image");
+    let orksLabel = document.createElement('p');
+    orksLabel.classList.add('forces-label');
+let factionChaos = document.createElement('div');
+factionChaos.classList.add("forces-select-image");
 factionChaos.id = "faction-chaos";
-    let chaosLabel = document.createElement('h2');
-    chaosLabel.id = "chaos-label";
+    let chaosLabel = document.createElement('p');
+    chaosLabel.classList.add('forces-label');
 let factionTyranids = document.createElement('div');
-factionTyranids.classList.add("forces-Select-Image");
+factionTyranids.classList.add("forces-select-image");
 factionTyranids.id = "faction-tyranids";
-    let tyranidsLabel = document.createElement('h2');
-    tyranidsLabel.id = "tyranids-label";
+    let tyranidsLabel = document.createElement('p');
+    tyranidsLabel.classList.add('forces-label');
 
 /*Appends*/
+enemiesContainer.appendChild(factionOrks);
+enemiesContainer.appendChild(factionChaos);
+enemiesContainer.appendChild(factionTyranids);
 factionOrks.appendChild(orksLabel);
 factionChaos.appendChild(chaosLabel);
 factionTyranids.appendChild(tyranidsLabel);
 
 /*Information Container Elements*/
 
+/*Text*/
+forcesTitle.innerHTML = "What Faction would you like to know more about?"
+spaceMarinesLabel.innerHTML = "Spacemarines";
+adMechLabel.innerHTML = "Adeptus Mechanicus";
+guardLabel.innerHTML = "Imperial Guard";
+orksLabel.innerHTML = "Orks";
+chaosLabel.innerHTML = "Chaos";
+tyranidsLabel.innerHTML = "Tyranids";
